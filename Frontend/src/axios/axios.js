64ada2d022017a86ff062430c35074a3ac33739c
vladimirhristovski <vladimir13hristovski@gmail.com>
2025-06-28 20:21:08 +0200

    commit5

--- Frontend/src/axios/axios.js
+++ Frontend/src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:9090/api",
+    baseURL: "http://ecommerce.local/api",
     headers: {
         "Content-Type": "application/json",
     },
